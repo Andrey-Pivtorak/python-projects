8fb23a41d62cedf12ac9fa061713701623f3d5f3
Andrey Pivtorak <77983534+Andrey-Pivtorak@users.noreply.github.com>
2021-02-03 15:51:34 +0200

    Create number_guessing.py

---
+++ number_guessing.py
@@ -0,0 +1,33 @@
+import random
+
+# global constants
+MIN_VALUE = 1
+MAX_VALUE = 10
+
+
+# main function
+def main():
+    number = random.randint(MIN_VALUE, MAX_VALUE)
+    user_guess(number)
+    print('Thank you for game!')
+
+
+# user_guess function
+def user_guess(number):
+    userNumber = int(input('Please, enter your number: '))
+    attemptСounter = 1
+    while userNumber != number:
+        if userNumber > number:
+            print('Too high, try again!')
+            userNumber = int(input('Please, enter your number: '))
+            attemptСounter += 1
+        elif userNumber < number:
+            print('Too low, try again!')
+            userNumber = int(input('Please, enter your number: '))
+            attemptСounter += 1
+    print('\nCongratulations. You guessed the number!')
+    print('You made', attemptСounter, 'attempts!')
+
+
+# call main function
+main()
